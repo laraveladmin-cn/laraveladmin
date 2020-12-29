966db1326291ba3395fcd155d2613736814027f5
zsping1989 <214986304@qq.com>
2020-12-29 16:38:45 +0800

    fix 检查并创建数据库

--- database/seeders/CheckDatabaseSeeder.php
+++ database/seeders/CheckDatabaseSeeder.php
@@ -12,13 +12,21 @@ class CheckDatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         $database = env('DB_DATABASE','forge');
         $sql = 'SELECT * FROM information_schema.SCHEMATA WHERE SCHEMA_NAME="'.$database.'" LIMIT 1';
-        $has = collect(DB::connection('schema')->select($sql))->first();
+        $has = false;
+        try {
+            $has = collect(DB::connection('schema')->select($sql))->first();
+        }catch (\Exception $exception){
+            $this->command->warn('请检查连接数据库用户是否有读取schema数据库权限!');
+        }
         if(!$has){
             $sql = 'CREATE DATABASE `'.$database.'` CHARACTER SET utf8';
-            DB::connection('schema')->statement($sql);
+            try{
+                DB::connection('schema')->statement($sql);
+            }catch (\Exception $exception){
+                $this->command->warn('创建'.$database.'数据库失败!');
+            }
         }
 
     }
