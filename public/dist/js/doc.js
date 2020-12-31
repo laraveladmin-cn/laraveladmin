4f3d851fb2d3508ce98e94387185bc19049b1ecc
zsping1989 <214986304@qq.com>
2020-12-31 15:08:35 +0800

    fix 加入评论系统

--- public/dist/js/doc.js
+++ public/dist/js/doc.js
@@ -50,18 +50,65 @@ window.$docsify = {
     logo: '/dist/img/logo.png',
     themeColor: '#3c8dbc',
     //disqus: 'shortname'
+    plugins:[
+        function(hook, vm) {
+            hook.init(function() {
+                // 初始化完成后调用，只调用一次，没有参数。
+                console.log(11);
+            });
+
+            hook.beforeEach(function(content) {
+                // 每次开始解析 Markdown 内容时调用
+                return content;
+            });
+
+            hook.afterEach(function(html, next) {
+                // 解析成 html 后调用。
+                // beforeEach 和 afterEach 支持处理异步逻辑
+                // ...
+                // 异步处理完成后调用 next(html) 返回结果
+                html = html+'<div id="comments">评论加载中...</div>';
+                next(html);
+            });
+
+            hook.doneEach(function() {
+                // 每次路由切换时数据全部加载完成后调用，没有参数。
+       /*         const giteement = new Giteement({
+                    id: 'demo page', // optional
+                    owner: 'laravel-admin',
+                    repo: 'laraveladmin',
+                    backcall_uri: 'https://www.laraveladmin.cn/home/index',
+                    oauth_uri: 'https://cors-anywhere.herokuapp.com/https://www.laraveladmin.cn/home/index',
+                    oauth: {
+                        client_id: '7173c415bd9e1d9f6d5660c1646da0aad295fd4266c34bf5c4ca9353298426ff',
+                        client_secret: 'c7bb795b6a6446f540f37126fc2eab14b960d8cef2cd9055362c38f1fa323fc9'
+                    },https://github.com/laraveladmin-cn/laraveladmin.git
+                    enable:true
+                });
+                giteement.render('comments');*/
+                const gitalk = new Gitalk({
+                    clientID: 'cc773b758ba981ee03c5',
+                    clientSecret: '1408f07f17667d34bd78947cdd597b2ae9cd62f5',
+                    repo: 'laraveladmin',
+                    owner: 'laraveladmin-cn',
+                    admin: ['laraveladmin-cn'],
+                    // facebook-like distraction free mode
+                    distractionFreeMode: true,
+                    id: location.hash.replace('#','')
+                });
+                gitalk.render('comments');
+            });
+
+            hook.mounted(function() {
+                // 初始化并第一次加载完成数据后调用，只触发一次，没有参数。
+            });
+
+            hook.ready(function() {
+                // 初始化并第一次加载完成数据后调用，没有参数。
+            });
+        }
+    ]
 
 };
-const giteement = new Giteement({
-    id: 'demo page', // optional
-    owner: 'laravel-admin',
-    repo: 'laraveladmin',
-    backcall_uri: 'https://www.laraveladmin.cn/home/index',
-    oauth_uri: 'https://cors-anywhere.herokuapp.com/https://www.laraveladmin.cn/home/index',
-    oauth: {
-        client_id: '7173c415bd9e1d9f6d5660c1646da0aad295fd4266c34bf5c4ca9353298426ff',
-        client_secret: 'c7bb795b6a6446f540f37126fc2eab14b960d8cef2cd9055362c38f1fa323fc9'
-    },
-});
-giteement.render('comments');
+
 
