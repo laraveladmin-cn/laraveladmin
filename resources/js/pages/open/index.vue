27d5dca55541686a21efa71a750724a96d62be2a
zsping1989 <214986304@qq.com>
2021-01-04 16:44:35 +0800

    fix 每天重置数据

--- resources/js/pages/open/index.vue
+++ resources/js/pages/open/index.vue
@@ -3,6 +3,7 @@
         官网首页
         <router-link to="/admin">后台</router-link>
         <router-link to="/home">前台</router-link>
+        <h3 style="color: red">请勿在本演示环境注册用户,本演示环境的注册,登录仅用于演示使用</h3>
         <h1>
             <a href="https://www.laraveladmin.cn">正式环境官网:https://www.laraveladmin.cn</a>
         </h1>
