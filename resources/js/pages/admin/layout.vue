36f6cbf0eb68be7c5b30c73cbb9932b801fb7fa7
zsping1989 <214986304@qq.com>
2021-01-09 12:57:50 +0800

    fix

--- resources/js/pages/admin/layout.vue
+++ resources/js/pages/admin/layout.vue
@@ -569,4 +569,10 @@
     body{
         padding-top: 0px;
     }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
 </style>
