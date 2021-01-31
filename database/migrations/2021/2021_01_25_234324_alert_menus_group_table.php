f90c2bac64180f57887a38284eca7bf072e7b64d
zsping1989 <214986304@qq.com>
2021-01-31 17:37:29 +0800

    fix

--- database/migrations/2021/2021_01_25_234324_alert_menus_group_table.php
+++ database/migrations/2021/2021_01_25_234324_alert_menus_group_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class AlertFirmMemberColumnsTable extends Migration
+class AlertMenusGroupTable extends Migration
 {
 
     protected $bindModel='App\Models\Menu';
@@ -21,7 +21,7 @@ class AlertFirmMemberColumnsTable extends Migration
         $prefix = $model->getConnection()->getTablePrefix();
         $connection = $model->getConnectionName()?: config('database.default');
         DB::connection($connection)->statement("ALTER TABLE `".$prefix.$model->getTable()."`
-ADD COLUMN `resource_id` int(0) UNSIGNED NOT NULL DEFAULT 0 COMMENT '所属资源' AFTER `status`,
+ADD COLUMN `resource_id` int(0) NOT NULL DEFAULT 0 COMMENT '所属资源' AFTER `status`,
 ADD COLUMN `group` varchar(100) NOT NULL DEFAULT '' COMMENT '后台路由所属组' AFTER `resource_id`,
 ADD COLUMN `action` varchar(255) NOT NULL DEFAULT '' COMMENT '绑定控制器方法' AFTER `group`,
 ADD COLUMN `env` varchar(100) NOT NULL DEFAULT '' COMMENT '使用环境' AFTER `action`,
