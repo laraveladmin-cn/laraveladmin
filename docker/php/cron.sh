2844ab0c0c165bea0e3cd1a0407c8da79e38ff70
zsping1989 <214986304@qq.com>
2021-03-10 16:49:58 +0800

    fix 修复定时任务不执行问题

--- docker/php/cron.sh
+++ docker/php/cron.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 code_dir=${LARAVEL_DIR}
+if [ "${code_dir}" = "" ]
+    then
+    code_dir="/var/www/laravel"
+fi
 projects=`ls $code_dir`
-
 for project in ${projects[*]}
 do
 cd ${code_dir}/${project} && /usr/local/bin/php ${code_dir}/${project}/artisan schedule:run >> /dev/null 2>&1 &
