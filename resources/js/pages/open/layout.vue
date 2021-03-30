cc177515c8df4cceab8c7bf9fb69e5ffca579954
zsping1989 <214986304@qq.com>
2021-03-30 11:05:47 +0800

    fix

--- resources/js/pages/open/layout.vue
+++ resources/js/pages/open/layout.vue
@@ -139,6 +139,12 @@
     };
 </script>
 <style scoped>
+    .navbar-fixed-top{
+        position:relative;
+    }
+    .navbar{
+        margin-bottom: 0px;
+    }
     @media screen and (min-width: 768px) {
         .open-content{
             min-height: 100%;
