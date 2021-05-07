c4286e68e3e442aa0c12b5da5b06a98e9ea24e3c
zsping1989 <214986304@qq.com>
2021-05-07 11:13:51 +0800

    fix

--- resources/js/pages/admin/layout.vue
+++ resources/js/pages/admin/layout.vue
@@ -232,7 +232,7 @@
                     <div class="pull-right hidden-xs">
                         <b>{{$t('System version:')}}</b> {{version}}
                     </div>
-                    <strong>Copyright &copy; 2020
+                    <strong>{{$t('Copyright')}}
                         <a target="_blank" href="http://www.laraveladmin.cn">{{name}}</a>
                     </strong>
                     <span v-if="icp">
