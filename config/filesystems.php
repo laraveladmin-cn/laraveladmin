09c74d549b54b3ffc299d80a9f64df0771ea6034
zsping1989 <214986304@qq.com>
2021-05-14 10:25:51 +0800

    fix 格式化代码

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,10 +51,10 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
-        'migrations'=>[
+        'migrations' => [
             'driver' => 'local',
             'root' => database_path('migrations'),
         ],
@@ -67,22 +67,22 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
         ],
-	'qiniu_public' => [
-	    'driver'        => 'qiniu',
-	    'domains'=>[
-	        'default'   => env('QINIU_DOMAIN_PUBLIC'),//你的七牛域名
-	        'https'     => '',         //你的HTTPS域名
-	        'custom'    => env('QINIU_DOMAIN_PUBLIC'),                //Useless 没啥用，请直接使用上面的 default 项
-	    ],
-	    'access_key'    => env('QINIU_ACCESS_KEY'),//AccessKey
-	    'secret_key'    => env('QINIU_SECRET_KEY'),//SecretKey
-	    'bucket'        => env('QINIU_BUCKET_PUBLIC','upload'),//Bucket名字(空间名称)
-	    'transport'     => env('QINIU_TRANSPORT','http'),//如果支持https，请填写https，如果不支持请填写http
-	    'visibility' => 'public',
-	    'access'    => 'public',  //空间访问控制 public 或 private
-	    'notify_url'=> '',  //持久化处理回调地址
-	    'hotlink_prevention_key' => null, // CDN 时间戳防盗链的 key。 设置为 null 则不启用本功能。
-	],
+        'qiniu_public' => [
+            'driver' => 'qiniu',
+            'domains' => [
+                'default' => env('QINIU_DOMAIN_PUBLIC'),//你的七牛域名
+                'https' => '',         //你的HTTPS域名
+                'custom' => env('QINIU_DOMAIN_PUBLIC'),                //Useless 没啥用，请直接使用上面的 default 项
+            ],
+            'access_key' => env('QINIU_ACCESS_KEY'),//AccessKey
+            'secret_key' => env('QINIU_SECRET_KEY'),//SecretKey
+            'bucket' => env('QINIU_BUCKET_PUBLIC', 'upload'),//Bucket名字(空间名称)
+            'transport' => env('QINIU_TRANSPORT', 'http'),//如果支持https，请填写https，如果不支持请填写http
+            'visibility' => 'public',
+            'access' => 'public',  //空间访问控制 public 或 private
+            'notify_url' => '',  //持久化处理回调地址
+            'hotlink_prevention_key' => null, // CDN 时间戳防盗链的 key。 设置为 null 则不启用本功能。
+        ],
 
     ],
 
