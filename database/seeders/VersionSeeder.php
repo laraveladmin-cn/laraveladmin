9610c07b4fc45c0aae4cc60a93967a1157775886
zsping1989 <214986304@qq.com>
2021-08-09 22:42:48 +0800

    fix 接口文档说明数据填充

--- database/seeders/VersionSeeder.php
+++ database/seeders/VersionSeeder.php
@@ -12,7 +12,8 @@ class VersionSeeder extends Seeder
     public function run()
     {
         $this->call(MenuTableSeeder::class); //菜单数据安装
-
+        //$this->call(ParamTableSeeder::class); //接口文档参数说明
+        //$this->call(ResponseTableSeeder::class); //接口文档响应说明
 
 
     }
