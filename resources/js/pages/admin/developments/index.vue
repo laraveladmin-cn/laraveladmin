0d77f531c4a9377056b7c5168dfecc2a5fbda342
zsping1989 <214986304@qq.com>
2021-09-17 11:06:52 +0800

    fix

--- resources/js/pages/admin/developments/index.vue
+++ resources/js/pages/admin/developments/index.vue
@@ -158,7 +158,7 @@
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" v-show="output">
                                     <div class="box">
                                         <div class="box-header with-border">
-                                            <h3 class="box-title">{{$tp('Execute results:')}}}</h3>
+                                            <h3 class="box-title">{{$tp('Execute results:')}}</h3>
                                             <div class="box-tools pull-right">
                                                 <button type="button" class="btn btn-box-tool" data-widget="collapse">
                                                     <i class="fa fa-minus"></i>
