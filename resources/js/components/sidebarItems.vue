df484f179e259d6b655d331df4af00856362a7d7
zsping1989 <214986304@qq.com>
2021-11-24 12:06:49 +0800

    fix 修复菜单折叠问题

--- resources/js/components/sidebarItems.vue
+++ resources/js/components/sidebarItems.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="treeview">
-        <a @click="toUrl(menu['url'],$event)" :href="menu['url']?menu['url']:null" :class="{'no-childrens':!(menu['childrens'] && menu['childrens'].length)}">
+        <a class="menu-a" @click="toUrl(menu['url'],$event)" :href="menu['url']?menu['url']:null" :class="{'no-childrens':!(menu['childrens'] && menu['childrens'].length)}">
             <i class="fa" :class="menu['icons']"></i>
             <span>{{$tp(menu['name'])}}</span>
             <span class="pull-right-container" v-if="menu['childrens']">
