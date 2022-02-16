2e07f37ec184ea40f6cf43409b7de47bc2697273
zsping1989 <214986304@qq.com>
2022-02-16 12:02:30 +0800

    fix

--- database/migrations/2022/2022_02_15_234324_alert_users_addr_table.php
+++ database/migrations/2022/2022_02_15_234324_alert_users_addr_table.php
@@ -18,46 +18,10 @@ class AlertUsersAddrTable extends Migration
         $model = new $this->bindModel();
         $prefix = $model->getConnection()->getTablePrefix();
         $connection = $model->getConnectionName()?: config('database.default');
-        DB::connection($connection)->statement("ALTER TABLE `".$prefix.$model->getTable()."`
+/*        DB::connection($connection)->statement("ALTER TABLE `".$prefix.$model->getTable()."`
 ADD COLUMN `addr` varchar(255) NOT NULL DEFAULT '' COMMENT '详细地址' AFTER `description`,
 ADD COLUMN `lng` decimal(10, 6) NULL DEFAULT NULL COMMENT '坐标经度' AFTER `addr`,
-ADD COLUMN `lat` decimal(10, 6) NULL DEFAULT NULL COMMENT '坐标纬度' AFTER `lng`;");
-        $sql = <<<'sql'
-CREATE DEFINER = `root` @`%` FUNCTION `GETDISTANCE` ( lat1 DOUBLE, lng1 DOUBLE, lat2 DOUBLE, lng2 DOUBLE ) RETURNS DOUBLE READS SQL DATA DETERMINISTIC BEGIN
-	DECLARE
-		RAD DOUBLE;
-	DECLARE
-		EARTH_RADIUS DOUBLE DEFAULT 6378137;
-	DECLARE
-		radLat1 DOUBLE;
-	DECLARE
-		radLat2 DOUBLE;
-	DECLARE
-		radLng1 DOUBLE;
-	DECLARE
-		radLng2 DOUBLE;
-	DECLARE
-		s DOUBLE;
-
-	SET RAD = PI( ) / 180.0;
-
-	SET radLat1 = lat1 * RAD;
-
-	SET radLat2 = lat2 * RAD;
-
-	SET radLng1 = lng1 * RAD;
-
-	SET radLng2 = lng2 * RAD;
-
-	SET s = ACOS( COS( radLat1 ) * COS( radLat2 ) * COS( radLng1 - radLng2 ) + SIN( radLat1 ) * SIN( radLat2 ) ) * EARTH_RADIUS;
-
-	SET s = ROUND( s * 10000 ) / 10000;
-	RETURN s;
-
-END
-sql;
-        DB::connection($connection)->statement($sql);
-
+ADD COLUMN `lat` decimal(10, 6) NULL DEFAULT NULL COMMENT '坐标纬度' AFTER `lng`;");*/
     }
 
     /**
