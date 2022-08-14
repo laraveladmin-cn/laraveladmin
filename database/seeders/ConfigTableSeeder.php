695771fdf4d716971d1fc3526944d753e55f94c6
zsping1989 <214986304@qq.com>
2022-08-14 22:36:24 +0800

    fix

--- database/seeders/ConfigTableSeeder.php
+++ database/seeders/ConfigTableSeeder.php
@@ -29,7 +29,7 @@ class ConfigTableSeeder extends Seeder
             'key' => 'system_version_no',
             'name' => '系统版本号',
             'description' => '系统版本号',
-            'value' => 'v1.0.0'
+            'value' => 'v1.0.1'
         ]);
 
         //百度统计代码
@@ -37,7 +37,7 @@ class ConfigTableSeeder extends Seeder
             'key' => 'baidu_statistics_url',
             'name' => '百度统计地址',
             'description' => '百度统计地址',
-            'value' => ''
+            'value' => 'https://hm.baidu.com/hm.js?5186d8e164fcab5b2f1a89af943269e8'
         ]);
 
 
