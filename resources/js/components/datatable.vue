68d5a345d7620fdc9e095c6d1bb331d5c501b104
zsping1989 <214986304@qq.com>
2022-09-25 22:09:13 +0800

    feature 新增定时自动刷新页面不显示加载状态

--- resources/js/components/datatable.vue
+++ resources/js/components/datatable.vue
@@ -321,7 +321,7 @@
                             </tr>
                             </tbody>
                             <tfoot >
-                            <tr class="loading" v-if="loading">
+                            <tr class="loading" v-if="loading && !hideLoading">
                                 <td colspan="6" class="overlay">
                                     <div class="fa">
                                         <i class="fa fa-refresh fa-spin"></i>
@@ -415,6 +415,7 @@
                 loading:false, //数据加载中状态
                 initLoading:true, //初始化加载状态
                 show_export_fields:false, //导出字段按钮显示状态
+                hideLoading:false,
                 data:{
                     options:{
                         where:{},
@@ -711,7 +712,8 @@
                 });
             },
             //刷新
-            refresh(){
+            refresh(hideLoading){
+                this.hideLoading = !!hideLoading;
                 let options = copyObj(this.affirm_options);
                 options['page'] = this.data.list['current_page'];
                 options['get_count'] = 1;
