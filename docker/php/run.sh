d2d410a3fa12ba04945e19bade955065e94f633e
zsping1989 <214986304@qq.com>
2022-10-02 16:48:35 +0800

    fix

--- docker/php/run.sh
+++ docker/php/run.sh
@@ -18,25 +18,25 @@ chmod 600 /var/spool/cron/crontabs/root
 projects=`ls ${code_dir}`
 #本地环境不自动启动队列
 if ! [ "${DOCKER_APP_ENV}" = "local" ]
-then
-##项目消息队列
-for project in ${projects}
-do
-    #导入配置
-    export $(grep -Ev '^$|[#;]' ${code_dir}/${project}/.env | xargs)
-    #判断是否本地开发环境
-    if [ "${APP_ENV}" = "" ] || [ "${APP_ENV}" = "local" ];
-        then
-            is_local=1
-            queue_type="listen"
-        else
-            is_local=0
-            queue_type="work"
-    fi
-    http_host=${SWOOLE_HTTP_HOST}
-    unset $(grep -Ev '^$|[#;]' ${code_dir}/${project}/.env | sed -E 's/([^=]*)=.*/\1/')
-#队列
-echo "[program:${project}]
+    then
+        ##项目消息队列
+        for project in ${projects}
+        do
+            #导入配置
+            export $(grep -Ev '^$|[#;]' ${code_dir}/${project}/.env | xargs)
+            #判断是否本地开发环境
+            if [ "${APP_ENV}" = "" ] || [ "${APP_ENV}" = "local" ];
+                then
+                    is_local=1
+                    queue_type="listen"
+                else
+                    is_local=0
+                    queue_type="work"
+            fi
+            http_host=${SWOOLE_HTTP_HOST}
+            unset $(grep -Ev '^$|[#;]' ${code_dir}/${project}/.env | sed -E 's/([^=]*)=.*/\1/')
+            #队列
+            echo "[program:${project}]
 process_name=%(program_name)s_%(process_num)02d
 command=php ${code_dir}/${project}/artisan queue:${queue_type} --sleep=3 --tries=3
 autostart=true
@@ -46,8 +46,8 @@ numprocs=1
 redirect_stderr=true
 stdout_logfile=${code_dir}/${project}/storage/logs/supervisor.log" > /etc/supervisor/conf.d/${project}.conf
 
-#队列监控
-#echo "[program:${project}_horizon]
+            #队列监控
+            #echo "[program:${project}_horizon]
 #process_name=%(program_name)s_%(process_num)02d
 #command=php ${code_dir}/${project}/artisan horizon
 #autostart=true
@@ -55,11 +55,11 @@ stdout_logfile=${code_dir}/${project}/storage/logs/supervisor.log" > /etc/superv
 #user=www-data
 #redirect_stderr=true
 #stdout_logfile=${code_dir}/${project}/storage/logs/horizon.log" > /etc/supervisor/conf.d/"${project}"_horizon.conf
-  #phpswoole服务
-  if  [ "${is_local}" = 0 ] && ! [ "${http_host}" = "" ]
-    then
-rm -f ${code_dir}/${project}/storage/logs/swoole_http.pid
-echo "[program:${project}_swoole]
+          #phpswoole服务
+          if  [ "${is_local}" = 0 ] && ! [ "${http_host}" = "" ]
+            then
+                rm -f ${code_dir}/${project}/storage/logs/swoole_http.pid
+                echo "[program:${project}_swoole]
 process_name=%(program_name)s_%(process_num)02d
 command=php ${code_dir}/${project}/artisan swoole:http start
 startsecs=3
@@ -70,9 +70,9 @@ user=www-data
 numprocs=1
 redirect_stderr=true
 stdout_logfile=${code_dir}/${project}/storage/logs/supervisor_swoole.log" > /etc/supervisor/conf.d/"${project}"_swoole.conf
-  fi
-done
-supervisord -c /etc/supervisor/supervisord.conf
+          fi
+        done
+        supervisord -c /etc/supervisor/supervisord.conf
 fi
 #supervisorctl stop all
 php-fpm
